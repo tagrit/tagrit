b353f3acd635d133632bb3ab4aba9307c5caeb73
davyamukowa <davyamukowa@gmail.com>
2025-02-14 17:49:39 +0300

    String or binary data would be truncated. fix the truncation issue

--- modules/pesapal/install.php
+++ modules/pesapal/install.php
@@ -7,12 +7,12 @@ if (!$CI->db->table_exists(db_prefix() . 'pesapal_txn')) {
 		`txn_id` bigint(40) NOT NULL,
 		`invoiceid` bigint(40) NOT NULL,
 		`amount` decimal(11,2) NOT NULL,
-		`reference_code` varchar(50) NOT NULL,
-		`tracking_id` varchar(5000) NOT NULL,
+		`reference_code` varchar(255) NOT NULL,
+		`tracking_id` varchar(10000) NOT NULL,
 		`txn_date` datetime NOT NULL,
 		`txn_ipn_date` datetime NOT NULL,
-		`notification_type` varchar(50) DEFAULT NULL,
-		`txn_status` varchar(20) NOT NULL,
+		`notification_type` varchar(100) DEFAULT NULL,
+		`txn_status` varchar(50) NOT NULL,
 		`flag` int(1) NOT NULL DEFAULT '0'
 	) ENGINE=InnoDB DEFAULT CHARSET=" . $CI->db->char_set . ';');
 
@@ -20,5 +20,4 @@ if (!$CI->db->table_exists(db_prefix() . 'pesapal_txn')) {
 		ADD PRIMARY KEY (`txn_id`);');
 	$CI->db->query('ALTER TABLE `' . db_prefix() . 'pesapal_txn`
 		MODIFY `txn_id` bigint(40) NOT NULL AUTO_INCREMENT;');
-
 }
