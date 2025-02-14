bb6b7d7ff362e599f8dac7b2c8fc91fb23346c2a
KEVINAMAYI <kevinamayi20@gmail.com>
2025-02-14 19:30:03 +0300

    change once in digital ocean...

--- modules/pesapal/libraries/OAuth.php
+++ modules/pesapal/libraries/OAuth.php
@@ -470,17 +470,9 @@ if (!class_exists("OAuthRequest")) {
          */
         private static function generate_nonce()
         {
-            $charid = strtoupper(md5(uniqid(rand(), true)));
-            $hyphen = chr(45); // "-"
-            $uuid = chr(123) .
-                substr($charid, 0, 8) . $hyphen .
-                substr($charid, 8, 4) . $hyphen .
-                substr($charid, 12, 4) . $hyphen .
-                substr($charid, 16, 4) . $hyphen .
-                substr($charid, 20, 12) .
-                chr(125); // "}"
-            return $uuid;
+            return substr(bin2hex(random_bytes(16)), 0, 32); // Exactly 32 characters
         }
+
     }
 }
 
