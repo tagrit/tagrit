9a16ef6adcdf17f7d507bb2bfbfe3ff4a616046a
KEVINAMAYI <kevinamayi20@gmail.com>
2025-03-20 11:56:10 +0300

    use php 8.2 for cpanel composer installl

---
+++ application/logs/log-2025-03-12.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-03-12 11:54:46 --> Could not find the language line "Marketing"
+ERROR - 2025-03-12 12:00:21 --> Could not find the language line "The unreconciled amount is <strong>300,000</strong>, which exceeds the threshold of <strong>200,000</strong>. Please reconcile the unreconciled funds before requesting more funds.</p>"
+ERROR - 2025-03-12 14:41:18 --> Could not find the language line "The unreconciled amount is <strong>300,000</strong>, which exceeds the threshold of <strong>200,000</strong>. Please reconcile the unreconciled funds before requesting more funds.</p>"
+ERROR - 2025-03-12 16:30:36 --> Could not find the language line "Marketing"
---
+++ application/logs/log-2025-03-20.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-03-20 08:45:05 --> Could not find the language line "Marketing"
+ERROR - 2025-03-20 08:45:05 --> Could not find the language line "Submit Request"
+ERROR - 2025-03-20 10:03:54 --> Could not find the language line "Marketing"
+ERROR - 2025-03-20 10:04:09 --> Could not find the language line "purchase_widget"
+ERROR - 2025-03-20 11:00:56 --> Could not find the language line "Marketing"
+ERROR - 2025-03-20 11:00:57 --> Could not find the language line "purchase_widget"
+ERROR - 2025-03-20 11:00:58 --> Could not find the language line "Marketing"
+ERROR - 2025-03-20 11:00:58 --> Severity: Warning --> Constant LB_API_DEBUG already defined C:\wamp64\www\tagrit\modules\purchase\libraries\gtsslib.php 3
+ERROR - 2025-03-20 11:00:58 --> Severity: Warning --> Constant LB_TEXT_CONNECTION_FAILED already defined C:\wamp64\www\tagrit\modules\purchase\libraries\gtsslib.php 6
+ERROR - 2025-03-20 11:00:58 --> Severity: Warning --> Constant LB_TEXT_INVALID_RESPONSE already defined C:\wamp64\www\tagrit\modules\purchase\libraries\gtsslib.php 7
+ERROR - 2025-03-20 11:00:58 --> Severity: Warning --> Constant LB_TEXT_VERIFIED_RESPONSE already defined C:\wamp64\www\tagrit\modules\purchase\libraries\gtsslib.php 8
+ERROR - 2025-03-20 11:00:58 --> Severity: 8192 --> Creation of dynamic property PurchaseLic::$check_interval_file is deprecated C:\wamp64\www\tagrit\modules\purchase\libraries\gtsslib.php 63
+ERROR - 2025-03-20 11:00:59 --> Could not find the language line "Marketing"
+ERROR - 2025-03-20 11:01:00 --> Could not find the language line "Marketing"
