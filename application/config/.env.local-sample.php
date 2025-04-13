7152ad4afb87b0e40e242868c8e9a1738b966248
kevin-amayi <kevinamayi20@gmail.com>
2025-04-13 11:45:15 +0300

    fix example .env file

--- application/config/.env.local-sample.php
+++ application/config/.env.local-sample.php
@@ -1,7 +1,7 @@
 <?php
-putenv('APP_BASE_URL=http://localhost/tagrit/');
-putenv('APP_DB_USERNAME=admin');
-putenv('APP_DB_PASSWORD=Crazy534105@@');
-putenv('APP_DB_NAME=tagrit_main');
+putenv('APP_BASE_URL=your-base-url'); //http://localhost/tagrit
+putenv('APP_DB_USERNAME=your-db-username'); //root
+putenv('APP_DB_PASSWORD=your-db-password'); //password
+putenv('APP_DB_NAME=your-db-name'); //tagrit
 putenv('APP_DB_HOSTNAME=localhost');
 putenv('APP_ENC_KEY=85bec75a1a6136881a01c08b1fdc31d8');
